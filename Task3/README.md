9c143cc0345d69afcb90df2ced3d3b4bc923f7b8
bhavana791 <80811054+bhavana791@users.noreply.github.com>
2021-11-28 13:42:44 +0530

    initial commit

---
+++ Task3/README.md
@@ -0,0 +1,1 @@
+# LGMVIP--Web-Development-task-3
---
+++ Task3/connection.php
@@ -0,0 +1,6 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$conn = mysqli_connect($servername, $username, $password);
+	$db = mysqli_select_db($conn,'gita');
